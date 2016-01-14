6fd0387acbaa1eca15d6243eb65c5823d54b78ba
PWBM <philip.michgelsen@gmail.com>
2016-01-14 12:24:14 +0100

    added crossover parameter to evolve

--- GPME_mfga_v2.hs
+++ GPME_mfga_v2.hs
@@ -5,10 +5,19 @@ import System.Random
 randomRange :: Random a => Int -> (a, a) -> Int -> [a]
 randomRange seed (a,b) k = take k $ randomRs (a,b) (mkStdGen seed)
 
+-- Generalisation of 'div' to any instance of Real (from Data.Fixed)
+div' :: (Real a,Integral b) => a -> a -> b
+div' n d = floor ((toRational n) / (toRational d))
+
+-- Generalisation of 'mod' to any instance of Real (from Data.Fixed)
+mod' :: (Real a) => a -> a -> a
+mod' n d = n - (fromInteger f) * d where
+    f = div' n d
+
 -- pairUp takes a list and pairs up every two elements.
 pairUp :: [a] -> [(a,a)]
 pairUp [] = []
-pairUp [_] = error "pairUp ERROR: not an even number in population!"
+pairUp [_] = error "pairUp ERROR: not an even number in population/generation!"
 pairUp (x:y:xs) = (x,y) : pairUp xs
 
 -- tripleUp takes a list and pairs up every three elements
@@ -50,14 +59,14 @@ mutationBinary seed p string = let
         mutation' (coinflip:restflips) (b:restbits) = error "mutationBinary ERROR: no binary string as input"
 
 -- this version of reproduction picks a reproduction result of a given size from a given population using the given seed.
-reproduction :: (Fractional a1, Integral b, Ord a1, Random a1) => Int -> [(a, b)] -> Int -> (a -> a1) -> [(a, a)]
+reproduction :: (Fractional a1, Integral b, Ord a1, Random a1) => Int -> [(a, b)] -> Int -> (a -> a1) -> [a]
 reproduction seed pop size fitnessfunction = let
         fitnesspop  = zip pop (map (fitnessfunction . fst) pop )                             -- gives a list [((organismtype, frequency), fitness)]
         totalfit    = sum $ map (\((x, freq), fit) -> fromIntegral freq * fit ) fitnesspop        -- the total fitness of the population
         fitProb     = map (\ ((x, freq),fit) -> fromIntegral freq * fit / totalfit ) fitnesspop   -- gives a list of probabilities for each organismtype, ordered as in pop.        
         popInterval = zip (map fst pop) (cumList fitProb)                            -- gives a list [(organismtype, interval)]
         coinflips   = randomRange seed (0.0,1.0) size                                -- coinflips to determine the reproduction result
-    in pairUp $ findStrings coinflips popInterval where
+    in findStrings coinflips popInterval where
         findStrings [] _ = []
         findStrings (flip:rest) popInterval = findStrings' flip popInterval : findStrings rest popInterval where
             findStrings' flip ((string,(a,b)):rest) = if b == 1
@@ -73,14 +82,15 @@ evolve :: (Fractional a, Integral b, Ord a, Random a) => Int                -- s
                                                         -> [([Char], b)]    -- population
                                                         -> Int              -- generation size
                                                         -> Int              -- maximum number of generations
+                                                        -> Float            -- crossover parameter
                                                         -> Float            -- mutation parameter
                                                         -> ([Char] -> a)    -- fitness function
                                                         -> [([Char], b)]    -- population after certain number of generations
-evolve seed pop gensize nrgen mpar fitnessfunction = let
+evolve seed pop gensize nrgen cpar mpar fitnessfunction = let
         seedsgen = map (`mod` 10000) (take nrgen (randoms $ mkStdGen seed :: [Int]))
     in evolve' seedsgen pop nrgen where
         evolve' _ pop 0 = pop
-        evolve' (s:seeds) pop k = evolve' seeds (createGen s pop gensize mpar fitnessfunction) (k-1)
+        evolve' (s:seeds) pop k = evolve' seeds (createGen s pop gensize cpar mpar fitnessfunction) (k-1)
 
 -- evolveVerbose runs the genetic algorithm verbosely
 evolveVerbose :: (Fractional a, Integral b, Ord a, Show b, Random a) => Int 
@@ -88,15 +98,16 @@ evolveVerbose :: (Fractional a, Integral b, Ord a, Show b, Random a) => Int
                                                                         -> Int 
                                                                         -> Int 
                                                                         -> Float 
+                                                                        -> Float
                                                                         -> ([Char] -> a) 
                                                                         -> IO ()
-evolveVerbose seed pop gensize nrgen mpar fitnessfunction = let 
+evolveVerbose seed pop gensize nrgen cpar mpar fitnessfunction = let 
         seedsgen = map (`mod` 10000) (take nrgen (randoms $ mkStdGen seed :: [Int]))
     in evolve' seedsgen pop nrgen where
         evolve' _ pop 0 = do putStrLn $ "Generation " ++ (show (nrgen)) ++ ": " ++ (show pop)
         evolve' (s:seeds) pop k = do
             putStrLn $ "Generation " ++ (show (nrgen - k)) ++ ": " ++ (show pop)
-            let newGen = createGen s pop gensize mpar fitnessfunction            
+            let newGen = createGen s pop gensize cpar mpar fitnessfunction            
             evolve' seeds newGen (k-1)
 
 
@@ -105,14 +116,18 @@ createGen :: (Fractional a, Integral b, Num t, Ord a, Random a) => Int
                                                                 -> [([Char], b)] 
                                                                 -> Int 
                                                                 -> Float 
+                                                                -> Float
                                                                 -> ([Char] -> a) 
                                                                 -> [([Char], t)]
-createGen seed pop gensize mpar fitnessfunction = let
-        [(seedPool, seedCross, seedMut)] = tripleUp $ map (`mod` 10000) (take 3 (randoms $ mkStdGen seed :: [Int]))
-        pool = reproduction seedPool pop gensize fitnessfunction
+createGen seed pop gensize cpar mpar fitnessfunction = let
+        [(seedPool, seedCross, seedMut)] = tripleUp $ map (`mod` 10000) (take 3 (randoms $ mkStdGen seed :: [Int]))        
+        pool = reproduction seedPool pop gensize fitnessfunction          -- base for the next generation
+        sizecrossoverpool = round $ (fromIntegral gensize)*cpar - (mod' ((fromIntegral gensize)*cpar) 2)
+        crossoverpool = pairUp $ take sizecrossoverpool pool
+        clonepool = drop sizecrossoverpool pool
         seedscross = take gensize (randoms $ mkStdGen seedCross)
         seedsmut = map (`mod` 10000) (take gensize (randoms $ mkStdGen seedMut :: [Int]))
-    in freqRep (mutate seedsmut (crossover' seedscross pool)) where
+    in freqRep (mutate seedsmut ((crossover' seedscross crossoverpool)++clonepool)) where
         crossover' _ [] = []
         crossover' (s:srest) ((a,b):prest)  = (crossover s a b) ++ (crossover' srest prest)
         mutate _ [] = []
