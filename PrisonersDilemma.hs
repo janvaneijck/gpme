66a78e4add5dfab429d7a5f58952eb185b11aace
Olim <olimtuyt@gmail.com>
2016-01-17 23:17:41 +0100

    added Prisoners Dilemma example, now fully working

--- PrisonersDilemma.hs
+++ PrisonersDilemma.hs
@@ -1,17 +1,20 @@
 module PrisonersDilemma where
 import Data.List
+import System.Random
+import GPME_mfga_v2 (evolve,evolveVerbose,freqRep) 
 
-type Move = Int
+type Move = Char
 type Game = [Move]
 type History = [Game]
 type Strategy = [Move]  -- a strategy will be a list of moves where the positions correspond directly
                         -- to a possible history, from the list possibleHistory.
+type Organism = String
 
 cc,cd,dc,dd :: Game
-cc = [1,1]
-cd = [1,0]
-dc = [0,1]
-dd = [0,0]
+cc = ['1','1']
+cd = ['1','0']
+dc = ['0','1']
+dd = ['0','0']
 
 -- possibleHistory takes an input representing the amount of games stored in the history and returns
 -- all possible histories. The order of this list will be used as a reference for the organisms in the
@@ -34,8 +37,8 @@ payoff game
 iteratedMatch :: Fractional a => Int -> Strategy -> Strategy -> History -> (a,a)
 iteratedMatch n strat1 strat2 history = iteratedMatch' n history (0,0) where
     iteratedMatch' 0 _ score = score
-    iteratedMatch' n [pgame, ppgame, pppgame] (scoreP1, scoreP2) = let
-            Just indexHistory       = elemIndex history (possibleHistory 3)
+    iteratedMatch' n history (scoreP1, scoreP2) = let
+            Just indexHistory       = elemIndex history (possibleHistory $ length history)
             moveP1                  = strat1 !! indexHistory
             moveP2                  = strat2 !! indexHistory
             (payoffP1, payoffP2)    = payoff [moveP1, moveP2]
@@ -44,8 +47,38 @@ iteratedMatch n strat1 strat2 history = iteratedMatch' n history (0,0) where
             newhistory              = [moveP1, moveP2] : (take (length history - 1) history)
         in iteratedMatch' (n-1) newhistory (newscoreP1, newscoreP2)
 
+-- this is the fitness function that tests a strategy against all strategies in the current population, using its
+-- own hypothetical three previous matches
+fitnessPD :: [(String,Int)] -> String -> Float
+fitnessPD pop (g1P1:g1P2:g2P1:g2P2:g3P1:g3P2:stratP1) = recurse pop where
+    starthistory = [[g1P1,g1P2],[g2P1,g2P2],[g3P1,g3P2]]
+    recurse [] = 0
+    recurse ((stratP2,_):rest) = (fst $ iteratedMatch 100 stratP1 (drop 6 stratP2) starthistory) + recurse rest
 
+-- intToChar converts a bit from type Int to type Char
+intToChar :: Int -> Char
+intToChar 0 = '0'
+intToChar 1 = '1'
+intToChar _ = undefined
 
+-- startPopulation takes a seed, an organism size and a population size and creates a random population of bitstrings
+-- of the right size using the given seed
+startPopulation :: Int -> Int -> Int -> [(Organism,Int)]
+startPopulation seed stringsize popsize = let
+        seeds = map (intToChar . flip (mod) 2) (take (stringsize * popsize) (randoms $ mkStdGen seed :: [Int]))
+    in freqRep $ startPopulation' seeds where
+        startPopulation' [] = []
+        startPopulation' seeds = take stringsize seeds : startPopulation' (drop stringsize seeds)
 
+-- randomStrat takes a seed and uses it to create a single strategy of length 64
+randomStrat :: Int -> Organism
+randomStrat seed = map (intToChar . flip (mod) 2) (take (64) (randoms $ mkStdGen seed :: [Int]))
 
-
+-- titfortatP2 takes the size of the history and returns the tit for tat strategy for player 2
+titfortatP2 :: Int -> Strategy
+titfortatP2 sizehist = titfortat (sizestring - 1) where
+        sizestring = 4^sizehist 
+        titfortat 0 = let prevgame = head $ (!!) (possibleHistory sizehist) (sizestring - 1) in 
+            if head prevgame == '0' then ['0'] else ['1']
+        titfortat n = let prevgame = head $ (!!) (possibleHistory sizehist) (sizestring - 1 - n) in 
+            if head prevgame == '0' then '0' : titfortat (n-1) else '1' : titfortat (n-1)
